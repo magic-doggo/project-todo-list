f6caa7ba8c819ffb7ab09cc9873e58a08ff6cc11
magic-doggo <109861946+magic-doggo@users.noreply.github.com>
2024-03-25 09:11:42 +0200

    Store user input as an object in toDoList array

--- src/index.js
+++ src/index.js
@@ -1,17 +1,40 @@
 import { toDo } from "./todo-class"
 
-console.log("asdasdasdasd")
+console.log("12345")
+
+let toDoList = []
+const newToDo = document.getElementById("new-todo")
+const dialog = document.getElementById("dialog")
+const cancelButton = document.getElementById("cancel-Btn")
+const confirmButton = document.getElementById("confirm-Btn")
+
+newToDo.addEventListener("click", () => {
+    dialog.showModal();
+})
+
+cancelButton.addEventListener("click", () => {
+    dialog.close("test cancel string where do i see this");
+})
+
+confirmButton.addEventListener("click", createToDo)
+
+function createToDo() {
+    let title = document.getElementById("title").value;
+    let description = document.getElementById("description").value;
+    let dueDate = document.getElementById("due-date").value;
+    let priority= document.getElementById("priority").value;
+    let newToDo = new toDo (title, description, dueDate, priority) 
+    toDoList.push(newToDo)  
+    console.log(toDoList)
+}
 
 // function createToDo(){
 //     let newToDo = new toDo(title, description, dueDate)
 // } first create button to request input from user for new todo. then take this input and add it through newToDo
-//title = document.getElementById("title").value? make sure id of title is correct
-//before that make an array of todos that this gets added to
-//later can create other smaller arrays depending on project
-//how will i select which project it belongs to? do I Need another parameter in toDo? dropdown to select the project? create a general project, or allow todos not to have proj?
-
-const newToDo = document.getElementById("new-todo")
-const dialog = document.getElementById("dialog")
+// title = document.getElementById("title").value? make sure id of title is correct
+// before that make an array of todos that this gets added to
+// later can create other smaller arrays depending on project
+// how will i select which project it belongs to? do I Need another parameter in toDo? dropdown to select the project? create a general project, or allow todos not to have proj?
 
 newToDo.addEventListener("click", () => {
     dialog.showModal();
