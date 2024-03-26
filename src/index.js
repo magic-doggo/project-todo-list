4a7731955fbe43557a2b6e4f926669e7bbbd12f6
magic-doggo <109861946+magic-doggo@users.noreply.github.com>
2024-03-26 07:37:41 +0200

    Make comfirm and close work properly and close the dialog when relevant

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ const newToDo = document.getElementById("new-todo")
 const dialog = document.getElementById("dialog")
 const cancelButton = document.getElementById("cancel-Btn")
 const confirmButton = document.getElementById("confirm-Btn")
+const form = document.getElementById("form")
 
 newToDo.addEventListener("click", () => {
     dialog.showModal();
@@ -21,23 +22,33 @@ confirmButton.addEventListener("click", createToDo)
 function createToDo() {
     let title = document.getElementById("title").value;
     let description = document.getElementById("description").value;
+    // let title2 = document.getElementById("title");
+
     let dueDate = document.getElementById("due-date").value;
     let priority = document.getElementById("priority").value;
     let project = document.getElementById("project").value;
     let newToDo = new toDo (title, description, dueDate, priority, project);
-    toDoList.push(newToDo);
-    console.log(toDoList);
+    if (form.checkValidity() === true){
+        toDoList.push(newToDo);
+        console.log(toDoList);
+        document.querySelector("form").reset();
+        dialog.close()
+        setRequiredToFalse() //if form is valid, setting required to false allows me to close the dialog window even though it is empty after clearing the form
+    }
     return newToDo;    
 }
 
-// function createToDo(){
-//     let newToDo = new toDo(title, description, dueDate)
-// } first create button to request input from user for new todo. then take this input and add it through newToDo
-// title = document.getElementById("title").value? make sure id of title is correct
-// before that make an array of todos that this gets added to
+function setRequiredToFalse(){
+    document.getElementById("title").required = false;
+}
+
+// make sure title is required when pressing submit
+
+
 // later can create other smaller arrays depending on project
 // how will i select which project it belongs to? do I Need another parameter in toDo? dropdown to select the project? create a general project, or allow todos not to have proj?
 
 newToDo.addEventListener("click", () => {
     dialog.showModal();
+    document.getElementById("title").required = true;
 })
